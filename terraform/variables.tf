6af41897a83afd872297498af014eb1bd1e4b4e5
Oleg Sidorenkov <oleg.sidorenkov.92@mail.ru>
2018-01-08 19:54:45 +0300

    Homework 08: first task with asterisk

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "project" {
+  type        = "string"
+  description = "infra-190310"
+}
+
+variable "region" {
+  type        = "string"
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable "zone" {
+  type        = "string"
+  description = "Zone"
+  default     = "europe-west1-b"
+}
+
+variable public_key_path {
+  description = "~/.ssh/appuser.pub"
+}
+
+variable private_key_path {
+  description = "~/.ssh/appuser"
+}
+
+variable "disk_image" {
+  type        = "string"
+  description = "reddit-base"
+}
